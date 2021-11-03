cdd3c90278fdb01e0648d0f77545b1ecb4661c07
Isachenkov26 <isachenkov.eduard26@gmail.com>
2021-11-03 16:55:31 +0200

    Modidied file

--- Lesson_1/Task_1_1/main.cpp
+++ Lesson_1/Task_1_1/main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hello Eduard!" << endl;
-    printf ("Good morning to you, good morning, good morning im glad to see you");
+    cout << ("Good morning to you, good morning, good morning im glad to see you") << endl;
     return 0;
 }
