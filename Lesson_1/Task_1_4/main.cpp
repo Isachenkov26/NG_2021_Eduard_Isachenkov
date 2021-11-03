5723aa425b33c5590e4959319aa0784fe9e9d09b
Isachenkov26 <isachenkov.eduard26@gmail.com>
2021-11-03 17:01:20 +0200

    Modidied file

--- Lesson_1/Task_1_4/main.cpp
+++ Lesson_1/Task_1_4/main.cpp
@@ -14,14 +14,15 @@ int main()
     cin >> c;
     D = b*b-4*a*c;
     if (D>0){
-    x1 = ((-b)+sqrt(D))/(2*a);
-    x2 = ((-b)-sqrt(D))/(2*a);
-    cout << "x1 = " << x1 << "\n";
-    cout << "x2 = " << x2 << "\n";
-}
+        x1 = ((-b)+sqrt(D))/(2*a);
+        x2 = ((-b)-sqrt(D))/(2*a);
+        cout << "x1 = " << x1 << "\n";
+        cout << "x2 = " << x2 << "\n";
+        }
     if (D==0){
         x1 = -(b/(2*a));
-        cout << "x1 = x2 = "<< x1;}
-        if (D<0)
-            cout << "D < 0, There are no roots.";
+        cout << "x1 = x2 = "<< x1;
+        }
+    if (D<0)
+        cout << "D < 0, There are no roots.";
 }
