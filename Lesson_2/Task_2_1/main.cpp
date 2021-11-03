e9c7ab0450528c1708a8e1394e113294a065b82c
Isachenkov26 <isachenkov.eduard26@gmail.com>
2021-11-03 17:24:31 +0200

    Modidied file

--- Lesson_2/Task_2_1/main.cpp
+++ Lesson_2/Task_2_1/main.cpp
@@ -4,17 +4,16 @@ using namespace std;
 
 int main()
 {
-    double x1, x2, x3, x4, x5, x6, sum;
-    cout << "Enter a random number (x<=9)"<< endl;
-    cin >> x1; cin >> x2; cin >> x3; cin >> x4; cin >> x5; cin >> x6;
-    if (x1<=9 && x2<=9 && x3<=9 && x4<=9 && x5<=9 && x6<=9)
-    {
-    sum=x1+x2+x3+x4+x5+x6;
-    cout <<"Sum=" << sum;
+    int number, sum=0;
+    cout << "Enter a random number = "<< endl;
+    cin >> number;
+    while (number>0){
+    sum += number%10;
+    number /= 10;
     }
-    else
-    {
-        cout << "Error";
-    }
-    return 0;
+    cout << "Sum =" << sum << endl;
+
+
+
+
 }
