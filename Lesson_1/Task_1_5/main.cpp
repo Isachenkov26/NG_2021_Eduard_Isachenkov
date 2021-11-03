a9e4495351e5e9700a4c3d1a6a1d5c6cfab861d5
Isachenkov26 <isachenkov.eduard26@gmail.com>
2021-11-03 16:23:48 +0200

    Added new file

--- Lesson_1/Task_1_5/main.cpp
+++ Lesson_1/Task_1_5/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
 int main()
 {
-    double massShip, planet, fuelShip;
+    double massShip=3000, planet, fuelShip;
     cout << "Good day" <<endl;
     cout << "Welcome to ZAZ, you are now on the ship ""Space Traveler"", which planet do you want to go to?" << endl;
     cin>>planet;
     cout << "We calculated. You will fly 300 light years, do not be afraid for you this flight will last only one week." << endl;
-    fuelShip = (trunc(massShip/3)) - 2;
-    cout << "Fuel for 300 light years required = 3331"<<endl;
+    fuelShip = ((massShip/3) - 2)*300;
+    printf("Fuel for 300 light years required = %f\n", fuelShip);
     cout << "Okay, let's start the timer" << endl;
     }
 
