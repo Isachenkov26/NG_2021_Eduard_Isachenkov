b46615f8ce81f8bba1ef9215f0c7c04495a4475d
Isachenkov26 <isachenkov.eduard26@gmail.com>
2021-11-08 16:10:58 +0200

    Modified file

--- Lesson_2/Task_2_1/main.cpp
+++ Lesson_2/Task_2_1/main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
     int number, sum=0;
-    cout << "Enter a random number = "<< endl;
+    cout << "Enter a random number = ";
     cin >> number;
     while (number>0){
-    sum += number%10;
-    number /= 10;
+            sum += number%10;
+            number /= 10;
     }
-    cout << "Sum =" << sum << endl;
+    cout << "Sum = " << sum << endl;
 }
