f2d3da0789ff220be55046015e1946be5f1e45b8
Isachenkov26 <isachenkov.eduard26@gmail.com>
2021-12-10 19:37:52 +0200

    Modified file 1.2

--- Lesson_3/Task_3_1/main.cpp
+++ Lesson_3/Task_3_1/main.cpp
@@ -4,20 +4,16 @@ using namespace std;
 
 int main()
 {
-    int arraying[10], newarray[10];
+    int arraying[10], newarray[10], number;
     cout << "Enter your numbers in the array" << endl;
-    for (int i=0; i<10; i++)
-    {
-      cin >> arraying[i];
+    for (int i=0; i<10; i++){
+        cin >> arraying[i];
     }
     cout << "Enter the number you want to add to all items" << endl;
-    int number;
     cin >> number;
-    for (int i=0; i<10; i++)
-        if(newarray[i]=arraying[i]+=number);
-    for (int i=0; i<10; i++)
-    {
-      cout << "Number[" << i << "]=" << newarray[i] << endl;
+    for (int i=0; i<10; i++){
+        newarray[i]=arraying[i]+=number;
+        cout << "Number [" << i << "]=" << newarray[i] << endl;
     }
     return 0;
 }
