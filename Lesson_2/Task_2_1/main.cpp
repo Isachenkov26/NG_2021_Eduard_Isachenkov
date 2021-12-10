f32f04b6f796eadb610a06d2735f660ba7281011
Isachenkov26 <isachenkov.eduard26@gmail.com>
2021-12-10 19:48:26 +0200

    Modified file 1.5

--- Lesson_2/Task_2_1/main.cpp
+++ Lesson_2/Task_2_1/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main()
 {
     int number, sum=0;
+    char jdi;
+    do {
     cout << "Enter a random number = ";
     cin >> number;
     while (number>0){
@@ -12,4 +14,7 @@ int main()
             number /= 10;
     }
     cout << "Sum = " << sum << endl;
+    cout << "If you want to change, press Y" << endl;
+    cin >> jdi;
+    }while (jdi == 'Y' || jdi == 'y');
 }
