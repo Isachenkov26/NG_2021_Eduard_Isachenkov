ec8c79aedee61efbf02f09813ab59d34b6f29c0d
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-01-13 11:45:20 +0200

    Fixed file 1.7

--- Lesson_1/Task_1_5/main.cpp
+++ Lesson_1/Task_1_5/main.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main()
 {
-    int fuelShip;
+    double MassShip;
     cout << "Good day" << endl;
     cout << "Welcome to \"ZAZ\", you are now on the ship \"Space Traveler\"" << endl;
+    cout << "Enter the mass of the ship" << endl;
+    cin >> MassShip;
     cout << "We calculated. You will fly 300 light years, do not be afraid for you this flight will last only one week." << endl;
-    fuelShip = ((3000/3) - 2)*300;
-    cout << "Fuel for 300 light years required = " << fuelShip << endl;
+    MassShip = ((MassShip/3.0) - 2.0)*300.0;
+    cout << "Fuel for 300 light years required = " << MassShip << endl;
     cout << "Okay, let's start the timer" << endl;
     return 0;
 }
