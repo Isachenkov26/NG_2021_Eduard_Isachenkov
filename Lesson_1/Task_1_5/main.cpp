9269468bbb9b289307e05030667b270565d8456a
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-01-22 19:11:45 +0200

    Added weight to the ship

--- Lesson_1/Task_1_5/main.cpp
+++ Lesson_1/Task_1_5/main.cpp
@@ -7,7 +7,7 @@ int main()
     double MassShip;
     cout << "Good day" << endl;
     cout << "Welcome to \"ZAZ\", you are now on the ship \"Space Traveler\"" << endl;
-    cout << "Enter the mass of the ship" << endl;
+    cout << "Enter the mass of the ship" << endl; // Added weight to the ship
     cin >> MassShip;
     cout << "We calculated. You will fly 300 light years, do not be afraid for you this flight will last only one week." << endl;
     MassShip = ((MassShip/3.0) - 2.0)*300.0;
