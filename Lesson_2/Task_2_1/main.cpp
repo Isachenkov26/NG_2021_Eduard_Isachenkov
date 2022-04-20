debd45eedd8fad35ac0c18e7f110f1614f571921
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-04-20 13:54:10 +0300

    Added file

--- Lesson_2/Task_2_1/main.cpp
+++ Lesson_2/Task_2_1/main.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main()
 {
-    int Number, Sum = 0;
-    char Request;
-    do {
-        cout << "Enter a random number = ";
-        cin >> Number;
-        cout << "If you want to change number, press Y." << endl;
-        cout << "If you do not want to change the number, press N." << endl;
-        cin >> Request;
-    }while (Request == 'Y' || Request == 'y');
-    while (Number>0){
-        Sum += Number%10;
-        Number /= 10;
+    int size;
+    cout << "Enter your square size: " << endl;
+    cin >> size;
+    for(int i  =  0; i < size; i++){
+        cout << '*'; // First column
+        for(int k = 0; k < size - 2; k++){
+            if (i == 0 || i == size - 1)
+                cout << "*"; // All other column
+            else
+                cout << " "; // Midst square
+             }
+        cout << '*' << endl; // Last column
     }
-    cout << "Sum = " << Sum << endl;
+    return 0;
 }
